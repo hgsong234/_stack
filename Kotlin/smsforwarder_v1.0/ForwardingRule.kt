77bacfa739e43d2264eed61b857d0dc835296c86
hgsong234 <hgs31497@gmail.com>
2025-09-09 09:15:15 +0900

    del

--- Kotlin/smsforwarder_v1.0/ForwardingRule.kt
+++
@@ -1,8 +0,0 @@
-package com.example.smsforwarder
-
-data class ForwardingRule(
-    val filterNumber: String,
-    val filterWord: String,
-    val substitutionRule: String,
-    val forwardingNumbers: List<String>
-)
